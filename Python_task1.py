33a684667fd8b5e8f4bea2e63cf382d04807928d
Gulnara Pirliyeva <gulnarapirliyeva@Gulnaras-MacBook-Air.local>
2020-06-29 00:30:07 -0400

    Added Task1- Python Assignment

---
+++ Python_task1.py
@@ -0,0 +1,40 @@
+#Question 1
+x,y,z = 1, 1.5, 'Great'
+
+#Question 2
+w= 1+5j
+v=7
+v, w= w,v
+print(v)
+
+#Question 3  - Swap two numbers using the third variable as the result name
+#A
+a=10
+b=a
+result=b
+print(result)
+
+#B - do the same task without using any third variable
+g,h=9,11
+g,h=h,g
+
+#Question 4
+Value_given= (input("Your first name: "))
+print(f"Your first name is: {Value_given} " )
+
+#Question 5
+Number_1= int(input("Choose a number between 1-10: "))
+Number_2= int(input("Choose a number between 1-10: "))
+z=30
+final_result=Number_1+Number_2+z
+print(final_result)
+              
+#Question 6
+r = 'USA'
+print(type(r))
+              
+#Question 7
+u=5
+u='Gulnara'
+print(u)
+#Yes, it will. Because Python keeps the variable`s latest assigned value. It will give the most recent executed value of the variable.
