4c90fb69e9d8e4ceb3ed5c795dd4e1e0dfe54f46
Gulnara Pirliyeva <gulnarapirliyeva@Gulnaras-MacBook-Air.local>
2020-06-30 13:29:47 -0400

    Adding Python Task 2 first three questions

---
+++ PyTask2_Q1.py
@@ -0,0 +1,12 @@
+i=input("Please enter a number: ")
+if i%3==0 and i%5==0:
+    print('Consultadd Python Training')
+    
+elif i%3==0:
+    print('Consultadd')
+
+elif i%5==0:
+        print('c ')
+
+
+
